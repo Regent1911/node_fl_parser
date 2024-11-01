803396271bd835b3f15ac83ab0a4f5715c0553e5
Regent1911 <regent.cheese@gmail.com>
2024-11-01 13:45:45 +0300

    rm my settings

--- settings/_db_settings.js
+++ settings/_db_settings.js
@@ -1,8 +1,8 @@
 const config = {
-	host: 'vh386.timeweb.ru',
-	user: 'cx14726_nodejs',
-	database: 'cx14726_nodejs',
-	password: 'nRxge7jckk9B8dW',
+	host: {hostname},
+	user: {username},
+	database: {database},
+	password: {password},
 }
 
 module.exports = config;
